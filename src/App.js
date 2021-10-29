2b2ddc31ec320573675f57f30f02877ad0a95ef6
sadrulj <jamil.sadrul@gmail.com>
2021-10-30 02:23:30 +0600

    first Commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import AuthProvider from "./context/AuthProvider";
+import Header from "./components/Shared/Header";
+import Footer from "./components/Shared/Footer";
+import Home from "./components/Home/Home/Home";
+import NotFound from "./components/Shared/NotFound";
+
+function App() {
+  return (
+    <div className="App">
+      <AuthProvider>
+        <Router>
+          <Header></Header>
+          <Switch>
+            <Route exact path="/">
+              <Home></Home>
+            </Route>
+            <Route path="/home">
+              <Home></Home>
+            </Route>
+            <Route path="*">
+              <NotFound></NotFound>
+            </Route>
+          </Switch>
+          <Footer></Footer>
+        </Router>
+      </AuthProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/hooks/useFirebase.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const useFireBase = () => {
+  return (
+    <div>
+      <h2>use firebase</h2>
+    </div>
+  );
+};
+
+export default useFireBase;
